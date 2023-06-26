8febbb3a3aaaf2c763c96c07d47658e9b7181d42
Ericson-Manalo <ericmana98@gmail.com>
2023-06-26 11:42:16 +0200

    imported in style section of each component general.scss works

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -16,5 +16,7 @@ export default {
 }
 </script>
 <style lang="scss">
+@use '../styles/general.scss' as*;
+
     
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -15,6 +15,7 @@ export default {
     },
 }
 </script>
-<style lang="">
+<style lang="scss">
+@use '../styles/general.scss' as*;
     
 </style>
