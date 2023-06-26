a9bfd8b6a19a7d019b8acf049b184f638017d415
Ericson-Manalo <ericmana98@gmail.com>
2023-06-26 16:01:11 +0200

    creating the first main section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,17 @@
 <template>
     <main>
-        <h2>
-            App Main
-        </h2>
+        <section class="music">
+            <div class="music-section">
+                <h2 class="h2-fs">
+                Music Is Life
+                </h2>
+                <hr>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus cursus egestas. Cras quis vehicula quam, a tempus velit. Ut feugiat augue in sodales luctus
+                </p>
+            </div>
+            
+        </section>
     </main>
 </template>
 <script>
@@ -15,7 +24,22 @@ export default {
     },
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 @use '../styles/general.scss' as*;
-    
+@use '../styles/partials/variables' as*;
+@use '../styles/partials/mixins.scss' as*;
+
+section.music{
+    background-color: $gray;
+    display: flex;
+    justify-content: center;
+}
+
+div.music-section{
+    width: 60%;
+    display: flex;
+    @include centering(center, center, column);
+    text-align: center;
+}
+
 </style>
