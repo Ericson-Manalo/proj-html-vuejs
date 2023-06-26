ebe6c56f53406f57cdd108e033b4a3fd8f4b25c6
Ericson-Manalo <ericmana98@gmail.com>
2023-06-26 21:55:50 +0200

    creating new section about latest band news, by copying similar properties on the last section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,31 @@
 <template>
     <main>
+        <!--MUSIC SECTION-->
         <section class="music">
             <div class="music-section">
+                <div></div>
                 <h2 class="h2-fs">
-                Music Is Life
+                    Music Is Life
                 </h2>
                 <hr>
-                <p>
+                <p class="line-height">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus cursus egestas. Cras quis vehicula quam, a tempus velit. Ut feugiat augue in sodales luctus
                 </p>
             </div>
-            
+        </section>
+
+        <!--LATEST BRAND SECTION-->
+        <section class="band">
+            <div class="band-section">
+                <div></div>
+                <h2 class="h2-fs">
+                    Latest Band News
+                </h2>
+                <hr>
+                <p class="line-height">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus cursus egestas. Cras quis vehicula quam, a tempus velit. Ut feugiat augue in sodales luctus
+                </p>
+            </div>
         </section>
     </main>
 </template>
@@ -31,21 +46,35 @@ export default {
 
 section.music{
     background-color: $gray;
-    display: flex;
-    justify-content: center;
+    position: relative;
 }
 
-div.music-section{
+div.music-section div{
+    
+}
+div.music-section div{
+    width: 23%;
+    height: 16px;
+    background-color: $red;
+    position: absolute;
+    top: 0;
+}
+
+div.music-section,
+div.band-section{
     width: 40%;
     display: flex;
     @include centering(center, center, column);
     text-align: center;
-    padding: 4rem 0;
+    padding: 5rem 0 4rem;
 }
 
-
-
-div.music-section p{
+div.music-section p,
+div.band-section{
     color: #7d7b8d;
+}
+
+section.band{
+    background-color: $darkgray;
 }
 </style>
