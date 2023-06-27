818c94831f290b9db712581f9220f42929930951
Ericson-Manalo <ericmana98@gmail.com>
2023-06-27 22:29:08 +0200

    creating first side of footer (upperside). created an objects of array that will be my list with v-for

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,8 +1,17 @@
 <template>
     <footer>
-        <h2>
-            App Footer
-        </h2>
+        <div class="upper-footer">
+            <div class="avada-logo">
+                <img src="../assets/images/logo_footer.png" alt="Avada's Logo">
+            </div>
+            <div class="list-settings">
+                <ul>
+                    <li v-for="setting in settingList">
+                        {{ setting.text }}
+                    </li>
+                </ul>
+            </div>
+        </div>
     </footer>
 </template>
 <script>
@@ -10,13 +19,44 @@ export default {
     name: 'AppFooter',
     data() {
         return {
-            
+            settingList:[
+                {
+                    text: 'Home',
+                    active: true
+                },
+                {
+                    text: 'Meet the Band',
+                    active: false
+                },
+                {
+                    text: 'Live Dates',
+                    active: false
+                },
+                {
+                    text: 'Latest News',
+                    active: false
+                },
+                {
+                    text: 'Albums',
+                    active: false
+                },
+                {
+                    text: 'Fans',
+                    active: false
+                }
+            ]
         }
     },
 }
 </script>
 <style lang="scss">
 @use '../styles/general.scss' as*;
+@use '../styles/partials/variables.scss' as*;
+@use '../styles/partials/mixins.scss' as*;
 
+
+div.upper-footer{
+    background-color: $gray;
+}
     
 </style>
